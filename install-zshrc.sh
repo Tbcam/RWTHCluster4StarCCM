b9e24169de12e94fb33e04c160f287604ab48913
Tbcam <102304809+Tbcam@users.noreply.github.com>
2025-06-08 03:17:52 +0200

    Update install-zshrc.sh
    real test

--- install-zshrc.sh
+++ install-zshrc.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 ab123=$(whoami)
-ZSHRC_PATH="/home/$USER_ID/.zshrc"
+ZSHRC_PATH="/home/$ab123/.zshrc"
 
+# Download .zshrc template
 curl -s https://raw.githubusercontent.com/Tbcam/RWTHCluster4StarCCM/main/.zshrc -o "$ZSHRC_PATH"
+
+# Replace all occurrences of 'ab123' with your actual username
+sed "s/ab123/$USER_ID/g" temp_zshrc > "$ZSHRC_PATH"
+
+# Clean up
+rm temp_zshrc
 
 echo ".zshrc updated for user: $USER_ID"
